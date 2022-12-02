16593aa6bae14419957bcd92833e3d6eca2ed09d
leoldding <leo.ld.ding@gmail.com>
2022-12-02 14:45:42 -0500

    created main page

---
+++ frontend/src/components/admin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/styles.css';
+import './css/admin.css';
+
+class Admin extends React.Component {
+    render() {
+        return (
+            <h1>Admin Login</h1>
+        )
+    }
+}
+
+export default Admin;
---
+++ frontend/src/components/main.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import './css/styles.css';
+import './css/main.css';
+
+class Container extends React.Component {
+    constructor(props) {
+        super(props);
+        this.colors = {
+            'Yes': 'yesBackground',
+            'En Route': 'enrouteBackground',
+            'No': 'noBackground',
+            'None': 'grayedBackground',
+        }
+    }
+
+    render() {
+        if (this.props.textAndColor !== '') {
+            return (
+                <div className={`container ${this.props.status === this.props.textAndColor ? this.colors[this.props.textAndColor] : this.colors['None']}`}>
+                    <p>{this.props.textAndColor}</p>
+                    <p>{this.props.status === this.props.textAndColor && this.props.subStatus}</p>
+                </div>
+            )
+        }  else {
+            return (
+                <div className={`container ${this.colors[this.props.status]}`}>
+                    <p>{this.props.status}</p>
+                    <p>{this.props.subStatus}</p>
+                </div>
+            )
+        }
+    }
+}
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mobile: true,
+            status: 'No',
+            subStatus: 'Temporary Text'
+        };
+    };
+
+    componentDidMount() {
+        if (window.matchMedia("(min-width: 769px)").matches) {
+            this.setState({mobile: false})
+        }
+        window.addEventListener('resize', (event) => {
+            this.setState({mobile: window.matchMedia("(max-width: 768px").matches})
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Is Leo at Think Coffee?</h1>
+                {this.state.mobile ?
+                    <div>
+                        <Container status={this.state.status} subStatus={this.state.subStatus} textAndColor={'Yes'}/>
+                        <Container status={this.state.status} subStatus={this.state.subStatus} textAndColor={'En Route'}/>
+                        <Container status={this.state.status} subStatus={this.state.subStatus} textAndColor={'No'}/>
+                    </div>
+                    :
+                    <div>
+                        <Container status={this.state.status} subStatus={this.state.subStatus} textAndColor={''}/>
+                    </div>
+                }
+            </div>
+        )
+    }
+}
+
+export default Main;
